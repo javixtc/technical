00136ab40d1b0865a44f37f6b871192e0d921098
Javier Vidal <javiervidal1985@gmail.com>
2025-03-21 10:45:37 +0100

    feat: add model ProductPricing

---
+++ src/main/java/com/inditex/technical/domain/model/ProductPricing.java
@@ -0,0 +1,20 @@
+package com.inditex.technical.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProductPricing {
+    private Long brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Long priceList;
+    private Long productId;
+    private Integer priority;
+    private BigDecimal price;
+    private String currency;
+}
