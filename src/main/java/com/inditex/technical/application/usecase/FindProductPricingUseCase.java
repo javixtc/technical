90cbccb5e9551c4a429d0cf586aed3d3d0485bd1
Javier Vidal <javiervidal1985@gmail.com>
2025-04-28 10:45:42 +0200

    feat: add business logic to the FindProductPricingUseCase

--- src/main/java/com/inditex/technical/application/usecase/FindProductPricingUseCase.java
+++ src/main/java/com/inditex/technical/application/usecase/FindProductPricingUseCase.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 
+import com.inditex.technical.domain.exceptions.PriceNotFoundException;
 import com.inditex.technical.domain.model.ProductPricing;
 import com.inditex.technical.domain.ports.ProductPricingRepositoryPort;
 import com.inditex.technical.domain.ports.ProductPricingServicePort;
@@ -19,6 +20,7 @@ public class FindProductPricingUseCase implements ProductPricingServicePort {
 
     @Override
     public ProductPricing findPrice(LocalDateTime startDate, Long productId, Long brandId) {
-        return repositoryPort.findPrice(startDate, productId, brandId);
+        return this.repositoryPort.findPrice(startDate, productId, brandId)
+            .orElseThrow(() -> new PriceNotFoundException("Precio no encontrado para productId: " + productId + ", brandId: " + brandId + ", fecha: " + startDate));
     }
 }
